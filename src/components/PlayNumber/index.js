f605680c1add3c445a350e58f95d1e1ebdac7f92
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-04-21 08:32:06 -0300

    :construction: feat: Added PlayNumber component

---
+++ src/components/PlayNumber/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { NumberItem } from './styles';
+
+const PlayNumber = props => (
+        <NumberItem style={{ backgroundColor: props.colors[props.status] }}>
+            {props.number}
+        </NumberItem>
+)
+
+export default PlayNumber;
