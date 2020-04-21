8fae89ffd9f21ce1c7700b20832eb1a7d6eafa1c
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-04-21 16:48:37 -0300

    :construction: chore: Change StarMatch component to Game component

---
+++ src/components/Game/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const NumberContainer = styled.div`
+    text-align: center;
+    padding: 10px;
+    width: 50%;
+    border: thin solid #ddd;
+`;
+
+export const StarsContainer = styled.div`
+    text-align: center;
+    width: 50%;
+    border: thin solid #ddd;
+`;
+
+export const GameContainer = styled.div`
+    max-width: 500px;
+    margin: 0 auto;
+`;
+
+export const Body = styled.div`
+    display: flex;
+`;
